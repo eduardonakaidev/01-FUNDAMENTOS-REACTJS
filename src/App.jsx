9ef786f8e48ee97e1b66e6039403f0e7e9b885ea
eduardo issao nakai frasson <eduardofrnkdev@gmail.com>
2023-07-20 14:10:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Post } from './Post'
+
+
+export default function App(){
+  return(
+     <div>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post author="Eduardo" content="sedadasdaaeddae"/>
+
+      
+     </div>
+
+  )
+}
